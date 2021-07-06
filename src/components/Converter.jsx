e7d186f19cea9ea2c43f15d30e4c573d5a3b4747
Lazy-ferret <a.veselov83@gmail.com>
2021-07-06 21:52:48 +0300

    Converter fixed

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -2,42 +2,36 @@ import React, { useState } from 'react'
 import './Converter.css'
 
 export default function Converter(props) {
-    const [form, setForm] = useState({ name: '', rgbValue: '', isValid: '' });
+    const [form, setForm] = useState({ rgbValue: null, error: null });
 
     const getRgb = (hex) => {
+        const checkValidity = (/^#[a-f\d]{6}$/i).test(hex);
+        if (!checkValidity) {
+            return null
+        }
         hex = hex.replace('#', '');
         const r = parseInt(hex.slice(0, 2), 16);
         const g = parseInt(hex.slice(2, 4), 16);
         const b = parseInt(hex.slice(4, 6), 16);
-
-        setForm(form => ({ ...form, rgbValue: `rgb(${(r)}, ${(g)}, ${(b)})` }))
+        return `rgb(${(r)}, ${(g)}, ${(b)})`;
     }
 
     const onFieldChange = (e) => {
-        const regexp = /^#[a-f\d]{6}$/i;
-        setForm(form => ({ ...form, name: e.target.value }))
-
-        if (regexp.test(e.target.value)) {
-            setForm(form => ({ ...form, isValid: true }))
-        } else {
-            setForm(form => ({ ...form, isValid: false }))
-        }
-    }
-
-    const onSubmit = (e) => {
         e.preventDefault();
-        if (!form.isValid) {
-            setForm(form => ({ ...form, rgbValue: 'Ошибка!!!' }))
-        } else {
-            getRgb(form.name);
+        if (e.target.value.length > 6) {
+            const rgb = getRgb(e.target.value);
+            if (rgb !== null) {
+                setForm((prev) => ({ ...prev, rgbValue: rgb, error: null }));
+                return;
+            }
+            setForm((prev) => ({ ...prev, error: true }));
         }
     }
 
     return (
-        <form
+        <div
             className='Converter'
-            style={{ backgroundColor: form.rgbValue === 'Ошибка!!!' ? 'red' : form.rgbValue }}
-            onSubmit={onSubmit}
+            style={{ backgroundColor: form.error ? 'red' : form.rgbValue }}
         >
             <input
                 className='Converter-Input'
@@ -45,8 +39,7 @@ export default function Converter(props) {
                 id="hex"
                 name="hex"
             />
-            <div className='Converter-Output'>{form.rgbValue}</div>
-        </form>
+            <div className='Converter-Output'>{form.error ? 'Ошибка!!!' : form.rgbValue}</div>
+        </div>
     )
 }
-
